1998725098848f47a864e1b6725c3b3f20235de3
Chris Fulstow <chris.fulstow@gmail.com>
2012-10-16 13:54:12 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Sharpshooter
+## Adventures in C#
+
+##License
+This project is open source under the [The MIT License (MIT)](http://www.opensource.org/licenses/mit-license.php)
---
+++ Sharpshooter/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Sharpshooter
+{
+    [TestFixture]
+    public class Variance
+    {
+        [Test]
+        public void Test1()
+        {
+            Herd<Zebra> zebras = new Herd<Zebra>();
+            ICollective<Zebra> zebras2 = new Herd<Zebra>();
+
+        }
+    }
+
+    // herd	of	zebras
+
+    public interface ICollective<T>
+    {
+        int Count { get; set; }
+    }
+
+    public class Herd<T> : ICollective<T> { }
+    public class Zebra { }
+}
